59acff0a8f4a55b82f31a6540f044d98e132f0ef
Hillel Arnold <helrond@hotmail.com>
2018-09-10 15:57:07 -0400

    debugging routines

--- storer/routines.py
+++ storer/routines.py
@@ -2,6 +2,8 @@ import logging
 from structlog import wrap_logger
 from uuid import uuid4
 
+from gemini import settings
+
 from storer.clients import FedoraClient, ArchivematicaClient
 from storer.models import Package
 
@@ -14,55 +16,56 @@ class StoreRoutineError(Exception): pass
 
 
 class StoreRoutine:
-    def __init__(self, fedora_client):
+    def __init__(self):
         self.log = logger.bind(transaction_id=str(uuid4()))
         self.am_client = ArchivematicaClient()
+        self.fedora_client = FedoraClient()
 
     def run(self):
-        print('{}file/?package_type={}'.format(settings.ARCHIVEMATICA['baseurl']), self.package_type)
-        for package in am_client.retrieve_paged(url):
-            if not Package.objects.filter(type=self.package_type, data.uuid=package['uuid']).exists():
-                fedora_uri = self.store_package(package)
-                if not fedora_uri:
+        url = 'file/?package_type={}'.format(self.package_type)
+        for package in self.am_client.retrieve_paged(url):
+            if not Package.objects.filter(type=self.package_type, data__uuid=package['uuid']).exists():
+                container = self.store_package(package)
+                if not container:
                     raise StoreRoutineError("Could not store {} with UUID {} in Fedora".format(self.package_type, package['uuid']))
                 else:
                     Package.objects.create(
                         type=self.package_type.lower(),
                         data=package
                     )
-                    response = self.send_callback(fedora_uri)
+                    response = self.send_callback(container)
 
     def store_package(self, package):
         # https://wiki.archivematica.org/AIP_structure
         # https://wiki.archivematica.org/DIP_structure
-        download = download_package(package)
-        container = store_container(package)
+        download = self.download_package(package)
+        container = self.store_container(package)
         if container:
-            if self.package_type == 'DIP:
+            if self.package_type == 'DIP':
                 for _file in package['objects']:
-                    store_binary(_file)
+                    self.store_binary(_file)
             elif self.package_type == 'AIP':
-                store_binary(download)
-            return container['uri']
+                self.store_binary(download)
+            return container
         else:
             raise StoreRoutineError("Could not create BasicContainer for DIP {}".format(package['uuid']))
 
-    def download_package(package_json):
+    def download_package(self, package_json):
         # Get the actual binaries from Archivematica (download endpoint?)
-        pass
+        return True
 
-    def store_container(package_json):
+    def store_container(self, package_json):
         # add metadata
-        pass
+        return True
 
-    def store_binary(binary_file):
+    def store_binary(self, binary_file):
         # store binary file, add metadata
-        pass
+        return True
 
     def send_callback(self, fedora_uri):
         # figure out the bag identifier (or some piece of data that ties back to bag store)
         # POST the URI and bag identifier to Aquarius
-        pass
+        return True
 
 
 class AIPStoreRoutine(StoreRoutine):
