b7601c7a7ccd7d98dea5287a6d19db687a622a9a
Hillel Arnold <helrond@hotmail.com>
2019-09-14 21:18:14 -0400

    cleaning up imports

--- storer/views.py
+++ storer/views.py
@@ -1,6 +1,5 @@
 import urllib
 
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
