b0eebaba9fc89b84fc4d92ea7db858bb0c25b609
Hillel Arnold <hillel.arnold@gmail.com>
2023-03-02 15:16:51 -0500

    improving UI

--- gemini/settings.py
+++ gemini/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'gemini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "storer", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
